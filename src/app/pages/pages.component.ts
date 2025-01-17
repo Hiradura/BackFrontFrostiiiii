10a5d54c257460929997ac900cfdb3100127fa6a
Hiradura <165997687+Hiradura@users.noreply.github.com>
2025-01-17 10:01:22 +0100

    Update pages.component.ts

--- src/app/pages/pages.component.ts
+++ src/app/pages/pages.component.ts
@@ -1,6 +1,8 @@
 
 import { Component, OnInit } from '@angular/core';
 import { BaseService } from '../base.service'; 
+import { Subscription } from 'rxjs';
+import { HttpErrorResponse } from '@angular/common/http';
 
 @Component({
   selector: 'app-pages',
@@ -8,21 +10,46 @@ import { BaseService } from '../base.service';
   styleUrls: ['./pages.component.css']
 })
 export class PagesComponent implements OnInit {
-  datas: any[] = [];  
-  cols: string[] = ['ID', 'Name', 'Value', 'Category', 'Timestamp'];  
+  names: any[] = [];  
+  cols: string[] = ['ID', 'Name', 'Capital', 'Population', 'Area', 'Flag'];  
 name: any;
+
+feliratkozas!: Subscription
+
 
   constructor(private base: BaseService) {}
 
-  ngOnInit(): void {
+//   ngOnInit(): void {
 
-    this.base.getDatas().subscribe(
-      (res) => {
-        this.name = res; 
-      },
-      (error) => {
-        console.error('Hiba történt az adatok betöltésekor!', error);
-      }
-    );
-  }
+//     this.base.getDatas().subscribe(
+//       (res) => {
+//         this.name = res; 
+//       },
+//       (error) => {
+//         console.error('Hiba történt az adatok betöltésekor!', error);
+//       }
+//     );
+//   }
+// }
+ngOnInit(): void{
+  this.getDatas()
 }
+
+
+getDatas() : void{
+
+  this.feliratkozas = this.base.getDatas().subscribe({
+    next:(res:any)=>{
+      this.name = res
+    },
+    error: (err: HttpErrorResponse) => {
+      console.log(err)
+    }
+  })
+
+
+}
+
+
+}
+
