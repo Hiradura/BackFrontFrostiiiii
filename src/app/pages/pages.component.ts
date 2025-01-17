83976e68e7fb6c5774ec74cfffe7092456c5b86a
Hiradura <165997687+Hiradura@users.noreply.github.com>
2025-01-17 04:20:56 +0100

    Update pages.component.ts

--- src/app/pages/pages.component.ts
+++ src/app/pages/pages.component.ts
@@ -1,16 +1,28 @@
-import { Component } from '@angular/core';
-import { BaseService } from '../base.service';
+
+import { Component, OnInit } from '@angular/core';
+import { BaseService } from '../base.service'; 
 
 @Component({
   selector: 'app-pages',
   templateUrl: './pages.component.html',
-  styleUrl: './pages.component.css'
+  styleUrls: ['./pages.component.css']
 })
-export class PagesComponent {
-Pages:any=[]
+export class PagesComponent implements OnInit {
+  datas: any[] = [];  
+  cols: string[] = ['ID', 'Name', 'Value', 'Category', 'Timestamp'];  
+name: any;
 
-constructor(private base:BaseService){
-  this.base.getDatas().subscribe(
-  (res)=>this.Pages=res)
-}
+  constructor(private base: BaseService) {}
+
+  ngOnInit(): void {
+
+    this.base.getDatas().subscribe(
+      (res) => {
+        this.name = res; 
+      },
+      (error) => {
+        console.error('Hiba történt az adatok betöltésekor!', error);
+      }
+    );
+  }
 }
